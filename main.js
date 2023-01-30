0e74a2e48c1eb88deb84924317ea8a687d030285
Surendrakumare <surendrakumare2001@gmail.com>
2023-01-30 09:09:16 +0530

    modified: index.html modified:   main.jsc modified:   main.js

--- main.js
+++ main.js
@@ -1,22 +1,45 @@
 const guessCountValue = document.getElementById("guessCount");
 const guessMsg = document.getElementById("guessResult");
+const chance = document.getElementById("chance");
+const congratsMsg = document.querySelector(".congratsMsg");
+const startbutn = (document.getElementById("startbutn").onclick = start);
 let min = 1,
   max = 20;
+let count, random, guessCount;
+document.getElementById("button").addEventListener("click", guessFun);
+document.getElementById("startbutn").addEventListener("click", start);
 
-const random = Math.floor(Math.random() * (max - min) + min);
-const btn = (document.getElementById("button").onclick = guessFun);
+function start() {
+  count = 4;
+  guessCount = 0;
+  document.getElementById("startbutn").textContent = "restart";
+  guessCountValue.innerText = "0";
+  chance.textContent = 5;
+  document.getElementById("guessleft").textContent = "";
+  random = getRandom();
+}
 
-let guessCount = 0;
+function getRandom() {
+  return Math.floor(Math.random() * (max - min) + min);
+}
+
 function guessFun() {
-  const input = document.getElementById("input").value;
-  guessCount += 1;
-  guessCountValue.innerText = guessCount;
-
-  if (input == random) {
-    guessMsg.textContent = "Good guess";
-  } else if (input < random) {
-    guessMsg.textContent = "Low";
-  } else if (input > random) {
-    guessMsg.textContent = "High";
+  if (count >= 0) {
+    const input = document.getElementById("input").value;
+    guessCount += 1;
+    guessCountValue.innerText = guessCount;
+    chance.textContent = count;
+    if (input == random) {
+      guessMsg.textContent = "Good guess";
+      congratsMsg.textContent = "You won !!";
+    } else if (input < random) {
+      guessMsg.textContent = "Low";
+    } else if (input > random) {
+      guessMsg.textContent = "High";
+    }
+    count -= 1;
+  } else {
+    document.getElementById("guessleft").textContent =
+      "You Exceeds your chances";
   }
 }
